9f82197d0b3edc2e96aed7a28378ed2a2892c950
MacBook <macbook@MacBooks-MacBook-Pro.local>
2024-10-03 16:21:39 +0530

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,38 @@
+const socket = io();
+
+if(navigator.geolocation){
+  navigator.geolocation.watchPosition((position)=>{
+    const{latitude,longitude}=position.coords;
+    socket.emit("send-location",{latitude,longitude});
+
+  },(err)=>{
+    console.log(err);
+  }),
+  {
+    enableHighAccuracy:true,
+    timeout:5000,
+    maximumAge:0
+  }
+}
+const map=L.map("map").setView([0,0],16);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
+  attribution:"Kartik"
+}).addTo(map);
+
+const marker={};
+socket.on("receive-location",(data)=>{
+  const {id,latitude,longitude}=data;
+  map.setView([latitude,longitude],10);
+  if(marker[id]){
+    marker[id].setLatLng([latitude,longitude]);
+  }
+  else{
+    marker[id]=L.marker([latitude,longitude]).addTo(map);
+  }
+})
+socket.on("User disconnected",(id)=>{
+  if(marker[id]){
+    map.removeLayer(marker[id]);
+    delete marker[id];
+  }
+});
